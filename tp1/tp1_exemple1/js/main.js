70a7d8a13b3a372985de32e1ee2e5ffd8da32da1
hunnybunnyv <valeriyaa.lapshina@gmail.com>
2023-03-09 16:00:03 +0100

    Fin tp1

--- tp1/tp1_exemple1/js/main.js
+++ tp1/tp1_exemple1/js/main.js
@@ -8,11 +8,8 @@ window.onload = startGame;
 function startGame() {
     canvas = document.querySelector("#myCanvas");
     engine = new BABYLON.Engine(canvas, true);
-
     scene = createScene();
-
     //let sphere = scene.getMeshByName("mySphere");
-
     // main animation loop 60 times/s
     engine.runRenderLoop(() => {
         scene.render();
@@ -23,26 +20,55 @@ function createScene() {
     let scene = new BABYLON.Scene(engine);
     
     // background
-    scene.clearColor = new BABYLON.Color3(1, 0, 1);
+    scene.clearColor = new BABYLON.Color3(0.9, 0.6, 0.9);
     // Create some objects 
     // params = number of horizontal "stripes", diameter...
-    let sphere = BABYLON.MeshBuilder.CreateSphere("mySphere", {diameter: 2, segments: 32}, scene);
-    sphere.position.y = 1;
+    let sphere = BABYLON.MeshBuilder.CreateSphere("mySphere", {diameter: 1.5, segments: 32}, scene);
+    sphere.position.x = -4 ;
+    sphere.position.y = 1 ;
+    sphere.position.z = -2;
+
+    let sphere2 = BABYLON.MeshBuilder.CreateSphere("mySphere2", {diameter: 2, segments: 32}, scene);
+    sphere2.position.x = 4 ;
+    sphere2.position.y = 1 ;
+    // sphere2.position.z = -1;
+
+    let sphere3 = BABYLON.MeshBuilder.CreateSphere("mySphere3", {diameter: 1, segments: 32}, scene);
+    sphere3.position.x = -4 ;
+    sphere3.position.y = 1 ;
+    sphere3.position.z = -4;
+
+    let roof = BABYLON.MeshBuilder.CreateCylinder("roof", {diameter: 1.3, height: 1.2, tesselation: 100});
+    roof.scaling.x = 0.75;
+    roof.rotation.z = Math.PI/2;
+    roof.position.y = 5;
+    roof.position.z = 5;
 
     // a plane
-    let ground = BABYLON.MeshBuilder.CreateGround("myGround", {width: 60, height: 60}, scene);
+    let ground = BABYLON.MeshBuilder.CreateGround("myGround", {width: 20, height: 20}, scene);
     //console.log(ground.name);
 
-     camera = new BABYLON.FreeCamera("myCamera", new BABYLON.Vector3(0, 5, -10), scene);
+    let camera = new BABYLON.FreeCamera("myCamera", new BABYLON.Vector3(0, 15, 20), scene);
    // This targets the camera to scene origin
    camera.setTarget(BABYLON.Vector3.Zero());
    //camera.rotation.y = 0.3;
    camera.attachControl(canvas);
    
     let light = new BABYLON.HemisphericLight("myLight", new BABYLON.Vector3(0, 1, 0), scene);
-    light.intensity = 0.3;
+    light.intensity = 0.4;
     // color of the light
-    light.diffuse = new BABYLON.Color3(1, 1, 1);
+    light.diffuse = new BABYLON.Color3(1, 0.9, 1);
+    
+    let light2 = new BABYLON.HemisphericLight("myLight2", new BABYLON.Vector3(4, 1, 0), scene);
+    light2.intensity = 0.4;
+    // color of the light
+    light2.diffuse = new BABYLON.Color3(0.7, 0, 0.7);    
+    
+    let light3 = new BABYLON.HemisphericLight("myLight3", new BABYLON.Vector3(-4, 1, 0), scene);
+    light3.intensity = 0.4;
+    // color of the light
+    light3.diffuse = new BABYLON.Color3(0.3, 0, 0.6);
+
     return scene;
 }
 
--- tp1/tp1_exemple2/js/main.js
+++ tp1/tp1_exemple2/js/main.js
@@ -24,7 +24,7 @@ function createScene() {
     let ground = BABYLON.MeshBuilder.CreateGround("myGround", {width: 60, height: 60, segments:50}, scene);
     let mirrorMaterial = new BABYLON.StandardMaterial("mirrorMaterial", scene);
     
-    mirrorMaterial.diffuseColor = new BABYLON.Color3(0.4, 1, 0.4);
+    mirrorMaterial.diffuseColor = new BABYLON.Color3(0.1, 0.1, 0.4);
     // no reflection on the ground, specular color = black...
     mirrorMaterial.specularColor = new BABYLON.Color3.Black;
 
@@ -44,7 +44,7 @@ function createScene() {
     let spheres = [];
     let sphereMaterials = [];
 
-    for(let i = 0; i < 10; i++) {
+    for(let i = 0; i < 11; i++) {
         spheres[i] = BABYLON.MeshBuilder.CreateSphere("mySphere" +i, {diameter: 2, segments: 32}, scene);
         spheres[i].position.x += 3*i -9;
         spheres[i].position.y = 2;
@@ -59,6 +59,7 @@ function createScene() {
     sphereMaterials[0].diffuseColor = new BABYLON.Color3(5, 0, 0);
     sphereMaterials[0].specularColor = new BABYLON.Color3(0, 0, 0);
    
+    
     sphereMaterials[1].ambiantColor = new BABYLON.Color3(0, 0.5, 0);
     sphereMaterials[1].diffuseColor = new BABYLON.Color3(5, 0, 1);
     sphereMaterials[1].specularColor = new BABYLON.Color3(0, 0, 3);
@@ -86,21 +87,26 @@ function createScene() {
     sphereMaterials[6].diffuseColor = new BABYLON.Color3(1, 0, 0);
     sphereMaterials[6].alpha = 0.5;
 
-    sphereMaterials[7].diffuseTexture = new BABYLON.Texture("images/coins.png", scene);
+    sphereMaterials[7].diffuseTexture = new BABYLON.Texture("images/HK.png", scene);
     // With .png textures that have some transparent pixels, we can
     // have the texture "see through" if we set the hasAlpha property to true
     sphereMaterials[7].diffuseTexture.hasAlpha = true;
-    sphereMaterials[7].emissiveColor = new BABYLON.Color3.Red;
+    sphereMaterials[7].emissiveColor = new BABYLON.Color3.Magenta;
+    sphereMaterials[7].diffuseTexture.vScale *= -5;
 
     sphereMaterials[8].ambientColor = new BABYLON.Color3(0, .3, 0);
     sphereMaterials[8].bumpTexture = new BABYLON.Texture("images/normal_map.jpg", scene);
     sphereMaterials[8].bumpTexture.level = 15.0;
 
     sphereMaterials[9].diffuseTexture = new BABYLON.VideoTexture("video", ["videos/michel.mp4"],scene);
-    sphereMaterials[9].diffuseTexture.vScale *= -1;
+    sphereMaterials[9].diffuseTexture.vScale *= 1;
+
+    sphereMaterials[10].ambientColor = new BABYLON.Color3(0, 0, 0);
+    sphereMaterials[10].diffuseTexture = new BABYLON.Texture("images/Normal.jpg", scene);
+    // sphereMaterials[10].bumpTexture.level = 15.0;
 
 
-    let camera = new BABYLON.FreeCamera("myCamera", new BABYLON.Vector3(0, 1, -30), scene);
+    let camera = new BABYLON.FreeCamera("myCamera", new BABYLON.Vector3(0, 10, -30), scene);
    // This targets the camera to scene origin
    //camera.setTarget(BABYLON.Vector3.Zero());
    camera.attachControl(canvas);
@@ -112,9 +118,9 @@ function createScene() {
 
     var light2 = new BABYLON.PointLight("myPointLight2", new BABYLON.Vector3(0, 3, -10), scene);
     light2.intensity = .5;
-    light2.diffuse = new BABYLON.Color3.Green;
+    light2.diffuse = new BABYLON.Color3(.5, .5, 1);
 
-    
+    /*
     let counter = 0;
 
     scene.registerBeforeRender(() => {
@@ -129,7 +135,7 @@ function createScene() {
         sphereMaterials[4].diffuseTexture.uOffset += 0.005;
         sphereMaterials[5].diffuseTexture.uScale += 0.03;
     })
-
+*/
     return scene;
 }
 
